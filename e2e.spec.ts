a7f0df84e4af4e5c48c205bb6e16cf33fb8c1628
Dani Rodríguez <dani@danirod.es>
2021-08-14 12:13:47 +0200

    feat: add a functional test bot to the library
    This functional test can be used to quickly check whether the library
    works, because it spawns a sample bot that can be used to quickly test
    integration against the library. Provide the required config as
    environment variables:
    
    * BOT_TOKEN: the token to login
    * ROLE: the role to grant the member when enters the voice channel
    * CHANNEL: the voice channel to monitor - the bot must be able to see it
    
    Make the bot connect into a guild that has a voice channel properly
    configured, and it should grant the user the role when joins the
    channel, and remove from the role when leaves.

---
+++ e2e.spec.ts
@@ -0,0 +1,44 @@
+/*
+ * This is a functional test to assert that the library behaves as
+ * it should. Yes, you should write unit tests, but your unit test
+ * will not talk to the real Discord. Run this script with ts-node
+ * and let the bot play to assert that it is working as expected.
+ * Also, you can use this file to learn how to use the library.
+ */
+
+import { Client, Intents } from "discord.js";
+import VoiceRoleManager from "./index";
+
+if (!process.env.BOT_TOKEN || !process.env.ROLE || !process.env.CHANNEL) {
+  console.error("Missing config");
+  process.exit(1);
+}
+const token: string = process.env.BOT_TOKEN;
+const role: string = process.env.ROLE;
+const channel: string = process.env.CHANNEL;
+
+const client = new Client({
+  ws: {
+    intents: Intents.FLAGS.GUILDS | Intents.FLAGS.GUILD_VOICE_STATES,
+  },
+});
+
+const manager = new VoiceRoleManager({
+  [channel]: [role],
+});
+
+client.on("ready", () => {
+  console.log("Bot is online");
+});
+
+client.on("voiceStateUpdate", (old, cur) => {
+  manager.trigger(old, cur);
+});
+
+client.login(token);
+process.on("SIGINT", () => {
+  client.destroy();
+});
+process.on("SIGTERM", () => {
+  client.destroy();
+});
